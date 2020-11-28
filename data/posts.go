fd2fe32e5ea33e2852d9ebb750b57caeb13d9144
Jimmy <jimmy.j@spenmo.com>
2020-11-29 00:23:47 +0530

    Added mysql database

--- data/posts.go
+++ data/posts.go
@@ -4,29 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"time"
 
 	"github.com/go-playground/validator/v10"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 //User is a struct that represents a User
 type User struct {
-	FullName  string `json:"fullName"`
-	UserName  string `json:"userName"`
-	Email     string `json:"email"`
-	CreatedOn string `json:"-"`
-	UpdatedOn string `json:"-"`
-	DeletedOn string `json:"-"`
+	FullName string `json:"fullName"`
+	UserName string `json:"userName"`
+	Email    string `json:"email"`
 }
 
 //Post is a struct that represents a post
 type Post struct {
-	Title     string `json:"title" validate:"required"`
-	Body      string `json:"body"`
-	Author    User   `json:"author"`
-	CreatedOn string `json:"-"`
-	UpdatedOn string `json:"-"`
-	DeletedOn string `json:"-"`
+	gorm.Model
+	Title  string `json:"title" validate:"required"`
+	Body   string `json:"body"`
+	Author User   `json:"author" gorm:"embedded;embeddedPrefix:author_"`
 }
 
 // Posts is a custom type which has an inbuilt toJson Method
@@ -52,71 +48,103 @@ func (p *Post) FromJSON(r io.Reader) error {
 
 // GetPosts returns all posts
 func GetPosts() Posts {
+
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       "root:@tcp(localhost:3306)/posts?charset=utf8&parseTime=True&loc=Local", // data source name, refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
+		DefaultStringSize:         256,                                                                     // add default size for string fields, by default, will use db type `longtext` for fields without size, not a primary key, no index defined and don't have default values
+		DisableDatetimePrecision:  true,                                                                    // disable datetime precision support, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,                                                                    // drop & create index when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,                                                                    // use change when rename column, rename rename not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false,                                                                   // smart configure based on used version
+	}), &gorm.Config{})
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var postList = []*Post{}
+	db.Find(&postList)
+
 	return postList
 }
 
 // AddPost takes a post object add add it to the postList
 func AddPost(post *Post) {
-	postList = append(postList, post)
+
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       "root:@tcp(localhost:3306)/posts?charset=utf8&parseTime=True&loc=Local", // data source name, refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
+		DefaultStringSize:         256,                                                                     // add default size for string fields, by default, will use db type `longtext` for fields without size, not a primary key, no index defined and don't have default values
+		DisableDatetimePrecision:  true,                                                                    // disable datetime precision support, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,                                                                    // drop & create index when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,                                                                    // use change when rename column, rename rename not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false,                                                                   // smart configure based on used version
+	}), &gorm.Config{})
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	db.Create(&post)
+	fmt.Println(post)
+
 }
 
 // UpdatePost will update the post and sends an error back if post is not found
 func UpdatePost(id int, post *Post) error {
-	if isPresent(id) {
-		postList[id] = post
-		return nil
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       "root:@tcp(localhost:3306)/posts?charset=utf8&parseTime=True&loc=Local", // data source name, refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
+		DefaultStringSize:         256,                                                                     // add default size for string fields, by default, will use db type `longtext` for fields without size, not a primary key, no index defined and don't have default values
+		DisableDatetimePrecision:  true,                                                                    // disable datetime precision support, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,                                                                    // drop & create index when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,                                                                    // use change when rename column, rename rename not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false,                                                                   // smart configure based on used version
+	}), &gorm.Config{})
+
+	if err != nil {
+		fmt.Print(err)
 	}
-	return ErrorPostNotFound
+
+	var tempData = &Post{}
+	dataFromDb := db.First(&tempData, id)
+
+	if dataFromDb.RowsAffected <= 0 {
+		return ErrorPostNotFound
+	}
+
+	tempData.Title = post.Title
+	tempData.Body = post.Body
+	tempData.Author = post.Author
+
+	db.Save(&tempData)
+	return nil
 }
 
 // DeletePost delete the post at index id. Returns Error in case post cannot be found
 func DeletePost(id int) error {
-	if isPresent(id) {
-		postList = append(postList[:id], postList[id+1:]...)
-		return nil
+
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       "root:@tcp(localhost:3306)/posts?charset=utf8&parseTime=True&loc=Local", // data source name, refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
+		DefaultStringSize:         256,                                                                     // add default size for string fields, by default, will use db type `longtext` for fields without size, not a primary key, no index defined and don't have default values
+		DisableDatetimePrecision:  true,                                                                    // disable datetime precision support, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,                                                                    // drop & create index when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,                                                                    // use change when rename column, rename rename not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false,                                                                   // smart configure based on used version
+	}), &gorm.Config{})
+
+	if err != nil {
+		fmt.Print(err)
 	}
-	return ErrorPostNotFound
+
+	var post = &Post{}
+	dataFromDb := db.First(&post, id)
+
+	if dataFromDb.RowsAffected <= 0 {
+		return ErrorPostNotFound
+	}
+
+	db.Delete(&post)
+	return nil
 }
 
 // ErrorPostNotFound is an error when post is not found in the list
 var ErrorPostNotFound = fmt.Errorf("Product not found")
-
-func isPresent(id int) bool {
-	if id >= len(postList) {
-		return false
-	}
-	return true
-}
-
-var postList = []*Post{
-	{
-		Title: "ONE",
-		Body:  "One Body",
-		Author: User{
-			FullName:  "One Author",
-			UserName:  "oneuser",
-			Email:     "one@gmail.com",
-			CreatedOn: time.Now().UTC().String(),
-			UpdatedOn: time.Now().UTC().String(),
-			DeletedOn: time.Now().UTC().String(),
-		},
-		CreatedOn: time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
-		DeletedOn: time.Now().UTC().String(),
-	},
-	{
-		Title: "Two",
-		Body:  "Two Body",
-		Author: User{
-			FullName:  "Two Author",
-			UserName:  "twouser",
-			Email:     "two@gmail.com",
-			CreatedOn: time.Now().UTC().String(),
-			UpdatedOn: time.Now().UTC().String(),
-			DeletedOn: time.Now().UTC().String(),
-		},
-		CreatedOn: time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
-		DeletedOn: time.Now().UTC().String(),
-	},
-}
